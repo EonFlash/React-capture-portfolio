eac5f3194a46e92f7a6b6938af9c173ec31729d3
EonFlash <gaurchintan@gmail.com>
2021-07-07 09:37:59 +0530

    Faq Section
    Created the Faq section in about us page
    No css right now

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
 
 //Page components
-import AboutSection from '../components/AboutSection'
-import ServiceSection from '../components/ServiceSection'
-
+import AboutSection from "../components/AboutSection";
+import ServiceSection from "../components/ServiceSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
-    return (
-        <>//* This acts like a div but does not render out like a div
-            <AboutSection />
-            <ServiceSection />
-            
-        </>
-    )
-}
+  return (
+    <>
+      //* This acts like a div but does not render out like a div
+      <AboutSection />
+      <ServiceSection />
+      <FaqSection />
+    </>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
